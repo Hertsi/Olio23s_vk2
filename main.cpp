61cc156872705f6e38907e666031b1cdeb28b435
Hertsi <127840594+Hertsi@users.noreply.github.com>
2023-09-05 23:39:24 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,10 @@
-#include <iostream>
 #include "game.h"
 
 int main() {
-    int maxNumber;
 
-    std::cout << "Aseta isoin arvattava luku: ";
-    std::cin >> maxNumber;
 
-    Game game(maxNumber);
-    game.play();
+    Game gameObject(20);
+    gameObject.play();
 
     return 0;
 }
